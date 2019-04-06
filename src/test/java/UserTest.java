bec1eb3fd74c49906f5b190cc9d71624ffb93a4e
lkarczewski <lkarczewski@sigma.ug.edu.pl>
2019-04-06 17:20:54 +0200

    User test with Hamcrest and AssertJ and JaCoCo config

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -10,29 +10,53 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UserTest {
 
     static User validUser;
-    static User badUser;
+    static User invalidUser;
     static String validLogin = "lkarczewski";
     static String validPassword = "haslo123";
-    static String badLogin = "";
-    static String badPassword = "ha";
+    static String invalidLogin = "";
+    static String invalidPassword = "ha";
 
     @BeforeAll
     public static void setUp() {
         validUser = new User(validLogin, validPassword);
-        badUser = new User(badLogin, badPassword);
+        invalidUser = new User(invalidLogin, invalidPassword);
     }
 
     //hamcrest
     @Test
     @DisplayName("validateUser() should return true with valid arguments")
-    void userValidationTestHamcrest() {
+    void userValidationTestTrueHamcrest() {
         assertThat(true, is(validUser.validateUser(validUser.getLogin(), validUser.getPassword())));
+    }
+
+    @Test
+    @DisplayName("validateUser() should return false with invalid arguments")
+    void userValidationTestFalseHamcrest() {
+        assertThat(false, is(invalidUser.validateUser(invalidUser.getLogin(), invalidUser.getPassword())));
+    }
+
+    @Test
+    @DisplayName("toString() should return login in string format")
+    void userToStringTestHamcrest() {
+        assertThat("'lkarczewski'", is(validUser.toString()));
     }
 
     //assertJ
     @Test
     @DisplayName("validateUser() should return true with valid arguments")
-    void userValidationTestAssertJ() {
+    void userValidationTestTrueAssertJ() {
         assertThat(validUser.validateUser(validUser.getLogin(), validUser.getPassword())).isTrue();
+    }
+
+    @Test
+    @DisplayName("validateUser() should return false with invalid arguments")
+    void userValidationTestFalseAssertJ() {
+        assertThat(invalidUser.validateUser(invalidUser.getLogin(), invalidUser.getPassword())).isFalse();
+    }
+
+    @Test
+    @DisplayName("toString() should return login in string format")
+    void userToStringTestAssertJ() {
+        assertThat(validUser.toString()).isEqualTo("'lkarczewski'");
     }
 }
