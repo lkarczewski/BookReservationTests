89ef44172d0e7d4309eb654f8a87f0137618a9dd
lkarczewski <lkarczewski@sigma.ug.edu.pl>
2019-04-06 15:01:16 +0200

    create User and update pom

---
+++ src/main/java/User.java
@@ -0,0 +1,30 @@
+public class User {
+
+    private String login;
+    private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean validateUser(String login, String password) {
+        if(login == null || login.length() == 0 || login.length() > 10 || password == null || password.length() == 0 || password.length() < 4) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "'" + login + "'";
+    }
+}
