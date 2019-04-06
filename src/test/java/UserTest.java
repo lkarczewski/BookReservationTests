743a2a8b6a50cbc25ce1706d8ee6185d3a09befc
lkarczewski <lkarczewski@sigma.ug.edu.pl>
2019-04-06 17:36:33 +0200

    Corrections to UserTest

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -2,24 +2,32 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.Matchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class UserTest {
 
     static User validUser;
-    static User invalidUser;
+    static User invalidUser1;
+    static User invalidUser2;
+    static User invalidUser3;
     static String validLogin = "lkarczewski";
     static String validPassword = "haslo123";
-    static String invalidLogin = "";
-    static String invalidPassword = "ha";
+    static String invalidLogin1 = null;
+    static String invalidPassword1 = null;
+    static String invalidLogin2 = "";
+    static String invalidPassword2 = "";
+    static String invalidLogin3 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    static String invalidPassword3 = "ha";
 
     @BeforeAll
     public static void setUp() {
         validUser = new User(validLogin, validPassword);
-        invalidUser = new User(invalidLogin, invalidPassword);
+        invalidUser1 = new User(invalidLogin1, invalidPassword1);
+        invalidUser2 = new User(invalidLogin2, invalidPassword2);
+        invalidUser3 = new User(invalidLogin3, invalidPassword3);
     }
 
     //hamcrest
@@ -32,7 +40,11 @@ public class UserTest {
     @Test
     @DisplayName("validateUser() should return false with invalid arguments")
     void userValidationTestFalseHamcrest() {
-        assertThat(false, is(invalidUser.validateUser(invalidUser.getLogin(), invalidUser.getPassword())));
+        assertAll("Invalid users tests", () -> {
+            assertThat(false, is(invalidUser1.validateUser(invalidUser1.getLogin(), invalidUser1.getPassword())));
+            assertThat(false, is(invalidUser2.validateUser(invalidUser2.getLogin(), invalidUser2.getPassword())));
+            assertThat(false, is(invalidUser3.validateUser(invalidUser3.getLogin(), invalidUser3.getPassword())));
+        });
     }
 
     @Test
@@ -51,7 +63,11 @@ public class UserTest {
     @Test
     @DisplayName("validateUser() should return false with invalid arguments")
     void userValidationTestFalseAssertJ() {
-        assertThat(invalidUser.validateUser(invalidUser.getLogin(), invalidUser.getPassword())).isFalse();
+        assertAll("Invalid users tests:", () -> {
+            assertThat(invalidUser1.validateUser(invalidUser1.getLogin(), invalidUser1.getPassword())).isFalse();
+            assertThat(invalidUser2.validateUser(invalidUser2.getLogin(), invalidUser2.getPassword())).isFalse();
+            assertThat(invalidUser3.validateUser(invalidUser3.getLogin(), invalidUser3.getPassword())).isFalse();
+        });
     }
 
     @Test
